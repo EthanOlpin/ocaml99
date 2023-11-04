64093d8fcf395cf5c05ea3ff1a5c0724729bcc8d
Ethan Olpin <olpin3@gmail.com>
2023-11-04 02:55:35 -0600

    solve problem 32

--- lib/arithmetic.ml
+++ lib/arithmetic.ml
@@ -5,3 +5,6 @@ let is_prime x =
   let rec aux i = i * i > x || (x % i <> 0 && aux (i + 1)) in
   x >= 2 && aux 2
 ;;
+
+(* Problem 32 *)
+let rec gcd a b = if b = 0 then a else gcd b (a % b)
--- test/arithmetic_test.ml
+++ test/arithmetic_test.ml
@@ -7,3 +7,9 @@ let%test_unit "is_prime" =
   [%test_eq: bool] (is_prime 7) true;
   [%test_eq: bool] (is_prime 12) false
 ;;
+
+(* Problem 32 *)
+let%test_unit "gcd" =
+  [%test_eq: int] (gcd 13 27) 1;
+  [%test_eq: int] (gcd 20536 7826) 2
+;;
